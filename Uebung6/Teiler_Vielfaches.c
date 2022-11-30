7e5ca289a37514001d68cdcc157a84fee7cbfa8f
Max <max.gerbeth03@gmail.com>
2022-11-30 17:13:00 +0100

    Teiler_Vielfaches.c

---
+++ Uebung6/Teiler_Vielfaches.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int ggT(int a, int b);
+int kgV(int a, int b);
+
+int main(){
+    printf("ggT(12, 10) = %d\n", ggT(12, 10));
+    printf("kgV(12, 10) = %d\n", kgV(12, 10));
+    
+    return 0;
+}
+
+int ggT(int a, int b){
+    //euklidischen Algorithmus
+    while(b != 0) {
+        int rest = a % b;
+        a = b;
+        b = rest;
+    }
+    return a;
+}
+
+int kgV(int a, int b){
+    return (a * b) / ggT(a, b);
+}
