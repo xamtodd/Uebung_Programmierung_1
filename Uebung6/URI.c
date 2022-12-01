67a4e199881b8236adfa1c7ed7359d3ae504dbf0
Max <max.gerbeth03@gmail.com>
2022-12-01 12:03:53 +0100

    URI.c Verbesserung

--- Uebung6/URI.c
+++ Uebung6/URI.c
@@ -9,42 +9,58 @@ void zeigeErgebnis(char, float);
 
 int main () {
     float u, r, i;
-    char modus = 'e';
-    printf("Welchen Wert wollen Sie berechnen?\n");
-    printf("U -> Spannung\n");
-    printf("R -> Widerstand\n");
-    printf("I -> Stromstaerke\n");
-    printf("Ihre Eingabe:");
-    scanf("%c", &modus);
-    printf("\n");
-    
-    modus = tolower(modus);
-    
-    switch(modus){
-        case 'u':
-            //Spannung
-            i = holeWert('I');
-            r = holeWert('R');
-            u = berechneSpannung(r, i);
-            zeigeErgebnis('U', u);
-            break;
-        case 'r':
-            //Wiederstand
-            i = holeWert('I');
-            u = holeWert('U');
-            r = berechneWiderstand(u, i);
-            zeigeErgebnis('R', r);
-            break;
-        case 'i':
-            //Stromstaeke
-            u = holeWert('U');
-            r = holeWert('R');
-            i = berechneStromstaeke(u, r);
-            zeigeErgebnis('I', i);
-            break;
-        default:
-            printf("Error\n");
-            return 0;
+    int modus = 0;
+    while(1){
+        printf("Welchen Wert wollen Sie berechnen? Geben Sie die Zahl ein:\n");
+        printf("U Spannung -> 1\n");
+        printf("R Widerstand -> 2\n");
+        printf("I Stromstaerke -> 3\n");
+        printf("Abbruch -> 0\n");
+        printf("Ihre Eingabe: ");
+        scanf("%d", &modus);
+        printf("\n");
+        
+        modus = tolower(modus);
+        
+        switch(modus){
+            case 1:
+                //Spannung
+                i = holeWert('I');
+                r = holeWert('R');
+                u = berechneSpannung(r, i);
+                zeigeErgebnis('U', u);
+                printf("\n");
+                printf("---------------------------------------------------\n");
+                printf("\n");
+                break;
+            case 2:
+                //Wiederstand
+                i = holeWert('I');
+                u = holeWert('U');
+                r = berechneWiderstand(u, i);
+                zeigeErgebnis('R', r);
+                printf("\n");
+                printf("---------------------------------------------------\n");
+                printf("\n");
+                break;
+            case 3:
+                //Stromstaeke
+                u = holeWert('U');
+                r = holeWert('R');
+                i = berechneStromstaeke(u, r);
+                zeigeErgebnis('I', i);
+                printf("\n");
+                printf("---------------------------------------------------\n");
+                printf("\n");
+                break;
+            case 0:
+                //Abbruch
+                printf("Abbruch!\n");
+                return 0;
+            default:
+                printf("Error\n");
+                return 0;
+        }
     }
     return 0;
 }
