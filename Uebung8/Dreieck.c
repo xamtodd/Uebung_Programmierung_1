77b14a4820a1f22079d826e333f02630c53af270
Max <max.gerbeth03@gmail.com>
2022-12-14 18:01:23 +0100

    Dreieck.c

---
+++ Uebung8/Dreieck.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void dreieck(int anz_zeilen);
+
+int main(){
+    dreieck(8);
+    
+    return 0;
+}
+
+void dreieck(int anz_zeilen){
+    //Geschsche Summenformel
+    int anzZeichen = (anz_zeilen * (anz_zeilen + 1)) / 2;
+    //Anzahl Zeichen + Anzahl \n
+    int gesZeichen = anzZeichen + anz_zeilen;
+    
+    char array[gesZeichen];
+    int index = 0;
+    for(int i = 0; i < anz_zeilen + 1; i++){
+        for(int j = 0; j < i; j++){
+            array[index] = 'x';
+            index++;
+        }
+        array[index] = '\n';
+        index++;
+    }
+    printf("%s", array);
+}
